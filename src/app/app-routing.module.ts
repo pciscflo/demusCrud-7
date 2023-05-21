1ca218c1fbf2b8a2e5444b7b5e41f80b3c483383
professor <pciscflo@upc.edu.pe>
2023-05-21 18:46:24 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthorComponent } from './component/author/author.component';
+import { CreateEditComponent } from './component/author/create-edit/create-edit.component';
+
+const routes: Routes = [
+  {
+   path: 'authors', component: AuthorComponent, children: [
+    {
+      path: 'nuevo', component: CreateEditComponent
+    },
+    {
+      path:'edicion/:id', component: CreateEditComponent
+    }
+   ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
